d9998102c9ef9d68ac5de3eaf2f684f10f23ca1a
harrison-mcadams <harrison.mcadams@gmail.com>
2019-07-08 09:06:31 -0400

    ensure mirrors are set to all off before and after acquisitions
    although this was likely the behavior ahead of time

--- code/protocols/+MRMaxFlash/run.m
+++ code/protocols/+MRMaxFlash/run.m
@@ -19,6 +19,8 @@ protocolParams.simulate.radiometer = true;
 %% Set up all the parameters and make Modulations
 [protocolParams,modDirection,background, nullDirection, ol] = MRMaxFlash.setAndSaveParams(protocolParams);
 
+% set OneLight to AllMirrorsOff
+ol.setAll(false);
 %% Make the temporal modulations for experiment
 [modulationsCellArray,temporalParams] = MRMaxFlash.makeTemporalModulations(modDirection,background,nullDirection,protocolParams);
 
@@ -28,5 +30,9 @@ protocolParams = MRMaxFlash.makeTrialOrder(protocolParams);
 %% Run the experiment.
 ApproachEngine(ol,protocolParams,modulationsCellArray,temporalParams,'acquisitionNumber',[],'verbose',protocolParams.verbose);
 
+% set OneLight to AllMirrorsOff. HMM notes that this step should be
+% unnecessary, as the behavior of ApproachEngine should be accomplishing
+% this automatically. However the extra line of code shouldn't hurt.
+ol.setAll(false);
 %% Post-Experiemnt Validations. 
 %MRMaxFlash.postExpValidation(protocolParams.nValidationsPerDirection,protocolParams,ol,modDirection,background,directions);
