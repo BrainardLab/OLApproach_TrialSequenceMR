ee0ed4df09bb12fdb959a7d959658be389060261
gkaguirre <gkaguirre@me.com>
2020-03-10 09:58:54 -0400

    Adjusting frequencies and acquisitions
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/protocols/+MRFlickerLDOG/run.m
+++ code/protocols/+MRFlickerLDOG/run.m
@@ -31,18 +31,57 @@ ol.setMirrors(backgroundStarts, backgroundStops);
 %%
 
 %% Define the trial orders
-trialOrders{1} = [2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4];
-trialOrders{2} = [2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4];
-trialOrders{3} = [2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4];
+% frequencySet = [0, 1/24, 1/12, 1/6];
+% We run the pupillometry with different stimulus frequencies for the
+% photoreceptor directions:
+%   L+S: 1/6 Hz
+%   LightFlux: 1/12 Hz
+%   RodMel: 1/12 Hz
+
+trialOrders{1} = [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4];
+trialOrders{2} = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3];
+trialOrders{3} = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3];
+trialOrders{4} = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3];
+trialOrders{5} = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3];
+trialOrders{6} = [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4];
+trialOrders{7} = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3];
+trialOrders{8} = [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4];
+trialOrders{9} = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3];
+trialOrders{10} = [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4];
+trialOrders{11} = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3];
+trialOrders{12} = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3];
+trialOrders{13} = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3];
+trialOrders{14} = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3];
+trialOrders{15} = [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4];
+trialOrders{16} = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3];
+trialOrders{17} = [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4];
+trialOrders{18} = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3];
+
 
 %% Define the contrast levels
 % These are always maximal
 contrastLevels = [1,1,1,1,1,1,1,1,1,1,1,1,1,1];
 
 %% Define the direction orders
-directionOrders{1} = 'LightFlux';
-directionOrders{2} = 'LplusS';
+directionOrders{1} = 'LplusS';
+directionOrders{2} = 'LightFlux';
 directionOrders{3} = 'RodMel';
+directionOrders{4} = 'LightFlux';
+directionOrders{5} = 'RodMel';
+directionOrders{6} = 'LplusS';
+directionOrders{7} = 'RodMel';
+directionOrders{8} = 'LplusS';
+directionOrders{9} = 'LightFlux';
+directionOrders{10} = 'LplusS';
+directionOrders{11} = 'LightFlux';
+directionOrders{12} = 'RodMel';
+directionOrders{13} = 'LightFlux';
+directionOrders{14} = 'RodMel';
+directionOrders{15} = 'LplusS';
+directionOrders{16} = 'RodMel';
+directionOrders{17} = 'LplusS';
+directionOrders{18} = 'LightFlux';
+
 
 
 %% Loop until we are done pupilling
@@ -50,7 +89,7 @@ stillMeasuring = true;
 while stillMeasuring
 
     % Which acquisition should we run?
-    acquisitionNumber = input('Enter acquisition number (1-3 pupil; 99 to stop measuring pupil): ');
+    acquisitionNumber = input('Enter acquisition number (1-18 pupil; 99 to stop measuring pupil): ');
     
     % Check if we are done scanning
     if acquisitionNumber==99
@@ -89,37 +128,62 @@ end
 
 
 %% Define the trial orders
-trialOrders{4} = [5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1];
-trialOrders{5} = [2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1];
-trialOrders{6} = [2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1];
-trialOrders{7} = [2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1];
-trialOrders{8} = [2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1];
-trialOrders{9} = [5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1];
-trialOrders{10} = [2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1];
-trialOrders{11} = [5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1];
-trialOrders{12} = [2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1];
+% frequencySet = [0, 4, 8, 16, 32, 64];
+% We use different stimulation frequencies for the different post-receptor
+% directions:
+%   L+S: 32 Hz
+%   L-S: 4 Hz
+%   RodMel: 4 Hz
+trialOrders{20} = [5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1];
+trialOrders{21} = [2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1];
+trialOrders{22} = [2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1];
+trialOrders{23} = [2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1];
+trialOrders{24} = [2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1];
+trialOrders{25} = [5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1];
+trialOrders{26} = [2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1];
+trialOrders{27} = [5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1];
+trialOrders{28} = [2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1];
+trialOrders{29} = [5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1];
+trialOrders{30} = [2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1];
+trialOrders{31} = [2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1];
+trialOrders{32} = [2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1];
+trialOrders{33} = [2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1];
+trialOrders{34} = [5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1];
+trialOrders{35} = [2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1];
+trialOrders{36} = [5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1];
+trialOrders{37} = [2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1];
 
 %% Define the contrast levels
 % These are always maximal
 contrastLevels = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
 
 %% Define the direction orders
-directionOrders{4} = 'LplusS';
-directionOrders{5} = 'LminusS';
-directionOrders{6} = 'RodMel';
-directionOrders{7} = 'LminusS';
-directionOrders{8} = 'RodMel';
-directionOrders{9} = 'LplusS';
-directionOrders{10} = 'RodMel';
-directionOrders{11} = 'LplusS';
-directionOrders{12} = 'LminusS';
+directionOrders{20} = 'LplusS';
+directionOrders{21} = 'LminusS';
+directionOrders{22} = 'RodMel';
+directionOrders{23} = 'LminusS';
+directionOrders{24} = 'RodMel';
+directionOrders{25} = 'LplusS';
+directionOrders{26} = 'RodMel';
+directionOrders{27} = 'LplusS';
+directionOrders{28} = 'LminusS';
+directionOrders{29} = 'LplusS';
+directionOrders{30} = 'LminusS';
+directionOrders{31} = 'RodMel';
+directionOrders{32} = 'LminusS';
+directionOrders{33} = 'RodMel';
+directionOrders{34} = 'LplusS';
+directionOrders{35} = 'RodMel';
+directionOrders{36} = 'LplusS';
+directionOrders{37} = 'LminusS';
+
 
 %% Loop until we are done scanning
 stillMeasuring = true;
 while stillMeasuring
 
     % Which acquisition should we run?
-    acquisitionNumber = input('Enter acquisition number (4-12 fMRI; 99 to stop scanning): ');
+    acquisitionNumber = input('Enter acquisition number (20-37 fMRI; 99 to stop scanning): ');
     
     % Check if we are done scanning
     if acquisitionNumber==99
