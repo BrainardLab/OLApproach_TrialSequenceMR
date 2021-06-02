e07354025ba6432145531b3e516d253805145aea
gkaguirre <gkaguirre@me.com>
2021-06-02 14:55:47 -0400

    Bug fix: Need to reverse the order of the deBruijn for odd runs

--- code/protocols/+MRAGTC/makeTrialOrder.m
+++ code/protocols/+MRAGTC/makeTrialOrder.m
@@ -50,7 +50,7 @@ if nDirections==5 && nContrasts==1 && nTrials==25
         deBruijn = [5,1,1,3,4,4,5,2,1,5,4,1,2,4,2,3,5,3,1,4,3,3,2,2,5];
         protocolParams.trialTypeOrder = [ones(1,nTrials);deBruijn];
     else
-        deBruijn = [5,1,1,3,4,4,5,2,1,5,4,1,2,4,2,3,5,3,1,4,3,3,2,2,5];
+        deBruijn = [5,2,2,3,3,4,1,3,5,3,2,4,2,1,4,5,1,2,5,4,4,3,1,1,5];
         protocolParams.trialTypeOrder = [ones(1,nTrials);deBruijn];
     end
     
