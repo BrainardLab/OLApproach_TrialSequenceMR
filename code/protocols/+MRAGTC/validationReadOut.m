6ec93597eb88b0e7a64f09f21b6ea6c675557017
gkaguirre <gkaguirre@me.com>
2021-06-07 12:11:25 -0400

    Simple script to report validations

---
+++ code/protocols/+MRAGTC/validationReadOut.m
@@ -0,0 +1,16 @@
+% A simple script to process the validation file and report contrasts
+
+
+directionNames = {'L-M','LMS','S','Omni'};
+receptorNames = {'L_2°','M_2°','S_2°','L_10°','M_10°','S_10°','rod_2°','rod_10°'};
+
+for dd = 1:4
+    for ii = 1:5
+        k(:,:,ii)=directedDirection{dd}.describe.validation(ii+10).contrastActual;
+    end
+    contrasts = round(100.*median(k,3),1);
+    fprintf(['\n' directionNames{dd} ':\n']);
+    for rr=1:length(receptorNames)
+        fprintf(['\t' receptorNames{rr} ': %2.1f, %2.1f \n'],contrasts(rr,:))
+    end
+end
